b16da094fa621c472d9793cf6933e5e718e2e5ea
Kir Ivanov <kirivanov777@gmail.com>
2026-07-08 00:03:17 +0300

    feat(front): add simple game selector

---
+++ frontend/src/components/dashboard-header.tsx
@@ -0,0 +1,67 @@
+import { useNavigate, useParams } from '@tanstack/react-router'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select'
+import { useQuery } from '@tanstack/react-query'
+import { Button } from './ui/button'
+
+export function DashboardHeader() {
+  const navigate = useNavigate()
+  const { id: gameId } = useParams({ strict: false })
+  const { data: games } = useQuery({
+    queryKey: ['game'],
+    queryFn: () => {
+      return [
+        { name: 'first', id: 'first' },
+        { name: 'test', id: 'test' },
+        { name: 'example', id: 'example' },
+      ]
+    },
+  })
+
+  const currentGameId: string | undefined = games?.find(
+    ({ id }) => id === gameId,
+  )?.id
+
+  function onGameChange(newValue: string) {
+    navigate({
+      to: '/dashboard/games/$id',
+      params: {
+        id: newValue,
+      },
+    })
+  }
+
+  function onLogout() {
+    navigate({
+      href: '/api/logout',
+    })
+  }
+
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex items-center gap-4">
+        <h1 className="font-heading text-2xl font-bold">Fame</h1>
+        <Select value={currentGameId} onValueChange={onGameChange}>
+          <SelectTrigger>
+            <SelectValue placeholder="Select a game" />
+          </SelectTrigger>
+          <SelectContent>
+            {games?.map((g) => (
+              <SelectItem key={g.id} value={g.id}>
+                {g.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+      <Button variant="destructive" onClick={onLogout}>
+        Log out
+      </Button>
+    </div>
+  )
+}
--- frontend/src/routes/dashboard.tsx
+++ frontend/src/routes/dashboard.tsx
@@ -1,50 +1,31 @@
-import { Button } from '#/components/ui/button'
+import { DashboardHeader } from '#/components/dashboard-header'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '#/components/ui/tabs'
 import { TooltipProvider } from '#/components/ui/tooltip'
-import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/dashboard')({
   component: DashboardLayout,
 })
 
-// function Dashboard() {
-//   return <TooltipProvider>
-//     <SidebarProvider
-//       style={
-//         {
-//           "--sidebar-width": "calc(var(--spacing) * 72)",
-//           "--header-height": "calc(var(--spacing) * 12)",
-//         } as React.CSSProperties
-//       }
-//     >
-//       <AppSidebar variant="inset" />
-//       <SidebarInset>
-//         <SiteHeader />
-//         <Outlet />
-//       </SidebarInset>
-//     </SidebarProvider>
-//   </TooltipProvider>
-// }
-
 function DashboardLayout() {
-  return <TooltipProvider>
-    <div className="px-4 mx-auto max-w-4xl">
-      <div className='flex justify-between mt-4'>
-        <nav className="flex gap-4">
-          <Button asChild>
-            <Link to='/dashboard'>Home</Link>
-          </Button>
-          <Button asChild>
-            <Link to='/dashboard/games'>My games</Link>
-          </Button>
-          <Button disabled>
-            <Link to='.'>Account</Link>
-          </Button>
-        </nav>
-        <Button variant='destructive'>Log out</Button>
+  return (
+    <TooltipProvider>
+      <div className="py-4 mx-auto max-w-7xl">
+        <DashboardHeader />
+        <main className="mt-4">
+          <nav>
+            <Tabs defaultValue="overview">
+              <TabsList>
+                <TabsTrigger value="overview">Overview</TabsTrigger>
+                <TabsTrigger value="analytics">Analytics</TabsTrigger>
+              </TabsList>
+              <TabsContent value="overview">this is overview</TabsContent>
+              <TabsContent value="analytics">this is analytics</TabsContent>
+            </Tabs>
+          </nav>
+          <Outlet />
+        </main>
       </div>
-      <main>
-        <Outlet />
-      </main>
-    </div>
-  </TooltipProvider>
+    </TooltipProvider>
+  )
 }
