1d0340529944a6bfd77a7219b5365d68a7bd127d
Kir Ivanov <kirivanov777@gmail.com>
2026-07-12 20:36:30 +0300

    chore(api): describe uuid to be stored as blob

--- backend/internal/db/models.go
+++ backend/internal/db/models.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"database/sql"
+
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
@@ -20,11 +22,11 @@ type User struct {
 type Game struct {
 	bun.BaseModel `bun:"table:games,alias:g"`
 
-	ID          uuid.UUID `bun:",pk"`
-	OwnerID     uuid.UUID `bun:"owner_id,notnull"`
-	DisplayName string    `bun:"display_name,notnull"`
-	GameURL     string    `bun:"game_url"`
-	ProjectName string    `bun:"project_name,notnull"`
+	ID          uuid.UUID      `bun:",pk,type:blob"`
+	OwnerID     uuid.UUID      `bun:"owner_id,notnull,type:blob"`
+	DisplayName string         `bun:"display_name,notnull"`
+	GameURL     sql.NullString `bun:"game_url"`
+	ProjectName string         `bun:"project_name,notnull"`
 
 	Boards []*Board `bun:"rel:has-many,join:id=game_id"`
 }
@@ -33,8 +35,8 @@ type Game struct {
 type Board struct {
 	bun.BaseModel `bun:"table:boards,alias:b"`
 
-	ID     uuid.UUID `bun:",pk"`
-	GameID uuid.UUID `bun:"game_id,notnull"`
+	ID     uuid.UUID `bun:",pk,type:blob"`
+	GameID uuid.UUID `bun:"game_id,notnull,type:blob"`
 	Name   string    `bun:",notnull"`
 }
 
