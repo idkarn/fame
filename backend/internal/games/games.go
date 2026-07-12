fd6bf83293b15906617b4748655a5d37972389f2
Kir Ivanov <kirivanov777@gmail.com>
2026-07-12 20:37:04 +0300

    feat(api): add route for creating new game

--- backend/internal/games/games.go
+++ backend/internal/games/games.go
@@ -1,5 +1,23 @@
 package games
 
-func GetGames() {
+import (
+	"context"
+	"fame/internal/db"
 
+	"github.com/google/uuid"
+)
+
+func NewGame(ctx context.Context, oid uuid.UUID, projectName string) (*db.Game, error) {
+	game := &db.Game{
+		ID:          uuid.Must(uuid.NewV7()),
+		OwnerID:     oid,
+		DisplayName: projectName,
+		ProjectName: projectName,
+	}
+
+	if _, err := db.Insert(game).Exec(ctx); err != nil {
+		return nil, err
+	}
+
+	return game, nil
 }
--- backend/internal/server/games.go
+++ backend/internal/server/games.go
@@ -1,7 +1,7 @@
 package server
 
 import (
-	"fame/internal/auth"
+	"fame/internal/games"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -9,11 +9,36 @@ import (
 )
 
 func newGame(c *echo.Context) error {
-	gid := uuid.NewString()
-	key := auth.NewKeyFromID(gid)
+	type newGameDTO struct {
+		ProjectName string `json:"projectName"`
+	}
+
+	var data newGameDTO
+	if err := c.Bind(&data); err != nil {
+		return c.String(http.StatusBadRequest, "wrong request format")
+	}
+
+	if data.ProjectName == "" {
+		return c.String(http.StatusBadRequest, "projectName must be provided")
+	}
+
+	uid, err := uuid.Parse(c.Get("user_id").(string))
+	if err != nil {
+		return echo.ErrBadRequest.Wrap(err)
+	}
+
+	// key := auth.NewKeyFromID(gid)
+	game, err := games.NewGame(c.Request().Context(), uid, data.ProjectName)
+	if err != nil {
+		return echo.ErrBadRequest.Wrap(err)
+	}
+
 	return c.JSON(http.StatusOK, map[string]any{
-		"gid": gid,
-		"key": key,
+		"id":          game.ID,
+		"ownerId":     game.OwnerID,
+		"displayName": game.DisplayName,
+		"gameURL":     game.GameURL.String,
+		"projectName": game.ProjectName,
 	})
 }
 
