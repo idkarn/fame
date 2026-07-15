3b22b6bbaec2961734271c46e584a2e6b53999f3
Kir Ivanov <kirivanov777@gmail.com>
2026-07-15 16:13:48 +0300

    feat(front): add boards api

---
+++ frontend/src/api/boards.ts
@@ -0,0 +1,32 @@
+import { DELETE, GET, PATCH, POST } from './client'
+
+export interface Board {
+  id: string
+  gameId: string
+  name: string
+}
+
+export async function getBoard(id: string): Promise<Board> {
+  return await GET(`/boards/${id}`)
+}
+
+export async function crateBoard(gameId: string, name: string): Promise<Board> {
+  return await POST('/boards/', {
+    gameId,
+    name,
+  })
+}
+
+interface BoardConfig {
+  name: string
+}
+export async function updateBoard(
+  id: string,
+  newBoard: BoardConfig,
+): Promise<string> {
+  return await PATCH(`/boards/${id}`, newBoard)
+}
+
+export async function deleteBoard(id: string): Promise<string> {
+  return await DELETE(`/boards/${id}`)
+}
